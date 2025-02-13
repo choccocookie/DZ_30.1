ebe066ed3fc626add2cdce68b56775e28f52345d
choccocookie <Karanovvlad@gmail.com>
2025-02-13 12:38:49 +0300

    Создайте новый Django-проект, подключите DRF в настройках проекта. Создвнв модель пользователь в приложении System Создвнв модель курс и урок в приложении materials CRUD для моделей курса с помощью ViewSet CRUD для моделей уроков с помощью Generic Изменен атрибут link... класса lesson

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,15 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import path
-from materials.views import CourseViewSet, LessonCreateApiView, LessonListApiView, LessonRetrieveApiView, LessonUpdateApiView, LessonDestroyeApiView
+from materials.views import (
+    CourseViewSet,
+    LessonCreateApiView,
+    LessonListApiView,
+    LessonRetrieveApiView,
+    LessonUpdateApiView,
+    LessonDestroyeApiView,
+)
 from materials.apps import MaterialsConfig
+
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
@@ -11,11 +19,10 @@ urlpatterns = [
     path("lesson/", LessonListApiView.as_view(), name="LessonList"),
     path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="LessonRetrieve"),
     path("lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="LessonUpdate"),
-    path("lesson/<int:pk>/delete/", LessonDestroyeApiView.as_view(), name="LessonDestroy"),
-    path("lesson/create/", LessonCreateApiView.as_view(), name="LessonCreate")
-
+    path(
+        "lesson/<int:pk>/delete/", LessonDestroyeApiView.as_view(), name="LessonDestroy"
+    ),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="LessonCreate"),
 ]
 
 urlpatterns += router.urls
-
-
