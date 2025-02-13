7d7bd6ef939465fefc0fef8106d0d7eaf1147f89
choccocookie <Karanovvlad@gmail.com>
2025-02-13 09:27:32 +0300

    Создайте новый Django-проект, подключите DRF в настройках проекта. Создвнв модель пользователь в приложении Users

---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    сity = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        help_text="Укажите город или иной населенный пункт",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Укажите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
