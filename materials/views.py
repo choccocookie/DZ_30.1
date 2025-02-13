d1225b183603dcd37597f5be1b3211ee58685523
choccocookie <Karanovvlad@gmail.com>
2025-02-13 11:28:26 +0300

    Создайте новый Django-проект, подключите DRF в настройках проекта. Создвнв модель пользователь в приложении System Создвнв модель курс и урок в приложении materials CRUD для моделей курса с помощью ViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from materials.models import Course
+from materials.serializers import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
